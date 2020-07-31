41ff8be6ce993f3d3138d5b0033826c06fb7140e
osnola2 <68616383+osnola2@users.noreply.github.com>
2020-07-31 02:05:32 -0300

    Create cpi8.py

---
+++ cpi8.py
@@ -0,0 +1,17 @@
+from toymodels import *
+from modal_functions import *
+
+def test8(i2, i3, i4, i5, i6, i7, i8):
+
+   for f2 in toy[i2]:
+       for f3 in toy[i3]:
+            for f4 in toy[i4]:
+                for f5 in toy[i5]:
+                    for f6 in toy[i6]:
+                        for f7 in toy[i7]:
+                            for f8 in toy[i8]:
+                                for i in K[8]:
+                                    for j in K[8]:
+                                        if ([modfun(f2, f3, f4, f5, f6, f7, f8, i[0], j[0]),
+                                             modfun(f2, f3, f4, f5, f6, f7, f8, i[1], j[1])]) not in K[8]:
+                                            print(f2, f3, f4, f5, f6, f7, f8)
